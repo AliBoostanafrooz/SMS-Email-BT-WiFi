86cc649662b1d4eaab426c083f386ad03581c4b8
Ali <ali.boostanafrooz@yahoo.com>
2019-06-05 13:05:01 +0430

    Premission Application for SMS/Email/ Turn ON/OFF BlueTooth and Wifi

--- app/src/main/java/com/example/premission/MainActivity.java
+++ app/src/main/java/com/example/premission/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.premission;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,13 +25,48 @@ public class MainActivity extends AppCompatActivity {
 
         PhoneNumber = findViewById(R.id.PhoneNumber);
         sms = findViewById(R.id.sendSMS);
-
-
         sms.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("sms"+PhoneNumber.getText().toString().trim())));
             }
         });
+
+        BT = findViewById(R.id.Bluetooth);
+        WF = findViewById(R.id.Wifi);
+       final BluetoothAdapter b = BluetoothAdapter .getDefaultAdapter();
+
+
+        if (b.isEnabled()){
+            BT.setChecked(true);
+
+        }else {
+            BT.setChecked(false);
+
+        }
+        BT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (BT.getText().equals("BLUETOOTH ON")) {
+
+                    b.enable();
+                    Toast.makeText(getApplication(), "بلوتوث روشن شد", Toast.LENGTH_LONG).show();
+
+                } else {
+                    b.disable();
+                    Toast.makeText(getApplication(), "  بلوتوث خاموش شد", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        } );
+
+
+
+
+
+
+
+
     }
 }
